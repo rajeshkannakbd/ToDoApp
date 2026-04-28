82d98c50dee48de3b8ea3fc9ec0e2fbeb2d3fad0
rajeshkanna <rajeshkannakbd@gmail.com>
2026-04-28 09:10:28 +0530

    backend url fix

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://todoappserver-tggh.onrender.com"
+  baseURL: "https://todoappserver-tggh.onrender.com/api"
 });
 
 export const getToday = (date) => API.get(`/today/${date}`);
